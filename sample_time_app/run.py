72bcf10e968d7d7ef4f160e7750546169f434c86
EthanTiancheng <153134380+EthanTiancheng@users.noreply.github.com>
2025-09-26 07:41:29 +0800

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,17 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello, Flask is running!"
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route("/time")
+def get_time():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {"time": now}
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
